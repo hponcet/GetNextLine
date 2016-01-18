ccb05e33910478333a314babb0a050f850d6d282
hponcet <hponcet@student.42.fr>
2016-01-18 20:47:27 +0100

    get_next_line version presque finale

--- get_next_line.c
+++ get_next_line.c
@@ -6,64 +6,64 @@
 /*   By: hponcet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/13 17:59:47 by hponcet           #+#    #+#             */
-/*   Updated: 2015/01/13 23:11:10 by hponcet          ###   ########.fr       */
+/*   Updated: 2016/01/18 20:42:25 by hponcet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "libft.h"
 #include <fcntl.h>
 
-int	get_next_line(int const fd, char **line)
+int		get_next_line(int const fd, char **line)
 {
 	char		*buf;
-	int		eof;
+	int			eof;
 	static char	*afternext;
 	char		*beforenext;
 
 	buf = ft_strnew(BUFF_SIZE + 1);
 	line[0] = ft_strnew(0);
-	if (ft_afternext(afternext, line) > 0)
+	if (ft_afternext(&afternext, line) > 0)
 		return (1);
 	while ((read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		if (ft_afternext(afternext, line) > 1)
-			return (1);
 		if ((eof = ft_cindex(buf, '\n')) >= 0)
 		{
 			beforenext = ft_strsub(buf, 0, eof);
 			line[0] = ft_strjoin(line[0], beforenext);
+			free(beforenext);
 			afternext = ft_strdup(buf + eof + 1);
+			free(buf);
 			return (1);
 		}
 		line[0] = ft_strjoin(line[0], buf);
 		ft_bzero(buf, BUFF_SIZE);
 	}
-	if ((read(fd, buf, BUFF_SIZE)) <= 0 || line == NULL)
+	if ((read(fd, buf, BUFF_SIZE)) < 0 || line == NULL)
 		return (-1);
 	return (0);
 }
 
-int	ft_afternext(char *afternext, char **line)
+int		ft_afternext(char **afternext, char **line)
 {
-	int	i;
+	int		i;
 	char	*tmp;
 
-	if (afternext)
+	if (*afternext != NULL)
 	{
-		i = ft_cindex(afternext, '\n');
+		i = ft_cindex(*afternext, '\n');
 		if (i >= 0)
 		{
-			line[0] = ft_strsub(afternext, 0, i);
-			tmp = ft_strsub(afternext, i + 1, (ft_strlen(afternext) - i + 1));
-			ft_strclr(afternext);
-			afternext = ft_strcpy(afternext, tmp);
+			line[0] = ft_strsub(*afternext, 0, i);
+			tmp = ft_strdup(*afternext + i + 1);
+			free(*afternext);
+			*afternext = tmp;
 			return (1);
 		}
 		else
 		{
-			line[0] = ft_strjoin(line[0], afternext);
-			ft_strclr(afternext);
+			line[0] = ft_strjoin(line[0], *afternext);
+			ft_bzero(*afternext, ft_strlen(*afternext));
+			*afternext = NULL;
 			return (0);
 		}
 	}
--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include "get_next_line.h"
-#include "libft.h"
+#include "libft/includes/libft.h"
 #include <fcntl.h>
 
 int	main(int argc, char **argv)
@@ -10,15 +10,14 @@ int	main(int argc, char **argv)
 
 	if (argc >= 2)
 		fd = open(argv[1], O_RDONLY);	
-	else
-	{
-		ft_putendl("Veuillez selectionner un fichier.");
-		return (0);
-	}
+	//else
+	//{
+		//ft_putendl("Veuillez selectionner un fichier.");
+		//return (0);
+	//}
 	i = 0;
-	while (get_next_line(fd, &(line)) > 0)
+	while (get_next_line(0, &(line)) > 0)
 		ft_putendl(line);
-	if (get_next_line(fd, &(line)) == -1)
-		ft_putendl("La fonction retourne une erreur");
 	return (0);
 }
+
