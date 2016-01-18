152646437e6735dec5351a92ac84e429dc8ccba2
Hugues Poncet <Gus@iMac.local>
2016-01-18 04:35:25 +0100

    get_next_line v1.0

--- get_next_line.c
+++ get_next_line.c
@@ -16,39 +16,30 @@
 
 int	get_next_line(int const fd, char **line)
 {
-	char		buf[BUFF_SIZE + 1];
+	char		*buf;
 	int		eof;
 	static char	*afternext;
 	char		*beforenext;
 
-	ft_bzero(buf, ft_strlen(buf));
-	DEBUG
+	buf = ft_strnew(BUFF_SIZE + 1);
 	line[0] = ft_strnew(0);
 	if (ft_afternext(afternext, line) > 0)
 		return (1);
 	while ((read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		DEBUG
-		ft_putendl("le buffer est :");
-		ft_putendl(buf);
 		if (ft_afternext(afternext, line) > 1)
 			return (1);
-		DEBUG
 		if ((eof = ft_cindex(buf, '\n')) >= 0)
 		{
-		DEBUG
 			beforenext = ft_strsub(buf, 0, eof);
 			line[0] = ft_strjoin(line[0], beforenext);
 			afternext = ft_strdup(buf + eof + 1);
-		DEBUG
 			return (1);
 		}
 		line[0] = ft_strjoin(line[0], buf);
 		ft_bzero(buf, BUFF_SIZE);
-		DEBUG
 	}
-	if ((read(fd, buf, BUFF_SIZE)) < 0 || line == NULL)
-		DEBUG
+	if ((read(fd, buf, BUFF_SIZE)) <= 0 || line == NULL)
 		return (-1);
 	return (0);
 }
@@ -58,15 +49,11 @@ int	ft_afternext(char *afternext, char **line)
 	int	i;
 	char	*tmp;
 
-		DEBUG
 	if (afternext)
 	{
-		DEBUG
 		i = ft_cindex(afternext, '\n');
-		ft_putnbr(i);
-		if (i > 0)
+		if (i >= 0)
 		{
-		DEBUG
 			line[0] = ft_strsub(afternext, 0, i);
 			tmp = ft_strsub(afternext, i + 1, (ft_strlen(afternext) - i + 1));
 			ft_strclr(afternext);
@@ -75,12 +62,10 @@ int	ft_afternext(char *afternext, char **line)
 		}
 		else
 		{
-		DEBUG
 			line[0] = ft_strjoin(line[0], afternext);
 			ft_strclr(afternext);
 			return (0);
 		}
 	}
-		DEBUG
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -12,9 +12,7 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 9999
-# include <stdio.h>
-# define DEBUG printf("__DEBUG ""%s ""%d""\n", __FILE__, __LINE__);
+# define BUFF_SIZE 32
 #include "libft.h"
 
 int	get_next_line(int const fd, char **line);
