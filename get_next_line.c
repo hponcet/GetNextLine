648cb1491c8f574e22dce511bbf568831d54bb0e
Hugues Poncet <Gus@iMac.local>
2016-01-18 02:00:51 +0100

    get_next_line.c fonctionnel -> pb BUFF_SIZE 9999

--- get_next_line.c
+++ get_next_line.c
@@ -10,42 +10,77 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 #include "libft.h"
+#include <fcntl.h>
 
 int	get_next_line(int const fd, char **line)
 {
-	int		ret;
 	char		buf[BUFF_SIZE + 1];
 	int		eof;
 	static char	*afternext;
 	char		*beforenext;
-	
-	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+
+	ft_bzero(buf, ft_strlen(buf));
+	DEBUG
+	line[0] = ft_strnew(0);
+	if (ft_afternext(afternext, line) > 0)
+		return (1);
+	while ((read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		if (afternext)
+		DEBUG
+		ft_putendl("le buffer est :");
+		ft_putendl(buf);
+		if (ft_afternext(afternext, line) > 1)
+			return (1);
+		DEBUG
+		if ((eof = ft_cindex(buf, '\n')) >= 0)
 		{
-			line[0] = ft_strcpy(afternext);
-			ft_bzero(afternext, ft_strlen(afternext));
-		}
-		if ((eof = ft_cindex(buf, '\n')) >= 0 || (eof = ft_cindex(buf, '\0')) >= 0)
-		{
-			if (!(beforenext = (char)malloc(sizeof(beforenext) * eof + 1)))
-				return (-1);
-			beforenext = ft_strncpy(buf, eof);
+		DEBUG
+			beforenext = ft_strsub(buf, 0, eof);
 			line[0] = ft_strjoin(line[0], beforenext);
-			afternext = ft_strsub(buf, eof, (ft_strlen(buf) - eof));
-			if (buf[eof] == '\0')
-				return (0);
-			else
-				return (1);
+			afternext = ft_strdup(buf + eof + 1);
+		DEBUG
+			return (1);
 		}
 		line[0] = ft_strjoin(line[0], buf);
 		ft_bzero(buf, BUFF_SIZE);
+		DEBUG
 	}
-	if (ft_cindex(afternext, '\0') >= 0) // erreur, afternext renvera toujours '\0'
+	if ((read(fd, buf, BUFF_SIZE)) < 0 || line == NULL)
+		DEBUG
+		return (-1);
+	return (0);
+}
+
+int	ft_afternext(char *afternext, char **line)
+{
+	int	i;
+	char	*tmp;
+
+		DEBUG
+	if (afternext)
 	{
-		line[0] = ft_strcpy(afternext);
-		return (0);
+		DEBUG
+		i = ft_cindex(afternext, '\n');
+		ft_putnbr(i);
+		if (i > 0)
+		{
+		DEBUG
+			line[0] = ft_strsub(afternext, 0, i);
+			tmp = ft_strsub(afternext, i + 1, (ft_strlen(afternext) - i + 1));
+			ft_strclr(afternext);
+			afternext = ft_strcpy(afternext, tmp);
+			return (1);
+		}
+		else
+		{
+		DEBUG
+			line[0] = ft_strjoin(line[0], afternext);
+			ft_strclr(afternext);
+			return (0);
+		}
 	}
-	return (-1)
+		DEBUG
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -5,16 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hponcet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/01/13 17:59:47 by hponcet           #+#    #+#             */
-/*   Updated: 2015/01/13 23:11:10 by hponcet          ###   ########.fr       */
+/*   Created: 2016/01/13 17:59:47 by hponcet           #+#    #+#             */
+/*   Updated: 2016/01/13 23:11:10 by hponcet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# ifdef GET_NEXT_LINE_H
-
-# define BUFF_SIZE 32
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 9999
+# include <stdio.h>
+# define DEBUG printf("__DEBUG ""%s ""%d""\n", __FILE__, __LINE__);
+#include "libft.h"
 
 int	get_next_line(int const fd, char **line);
+int	ft_afternext(char *afternext, char **line);
 
 #endif
